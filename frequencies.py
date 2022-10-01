49cd46385d9d3639bc38fae982c766d09b336561
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2022-10-01 14:02:34 +0100

    add solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,13 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if str(item) not in frequencies:
+            frequencies[str(item)] = 1
+        else:
+            frequencies[str(item)] = frequencies.get(str(item)) + 1
+
     return frequencies
